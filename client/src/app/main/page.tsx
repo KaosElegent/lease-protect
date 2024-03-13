6bac2f8692b9542dfcb943ce24d4bf6225f3bba4
Joao Vitor <jvtopanotti-da-cunha@myseneca.ca>
2024-03-13 19:46:41 -0400

    feat: auth0 able to login; use info is displayed

--- client/src/app/main/page.tsx
+++ client/src/app/main/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from 'next/image'
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
@@ -8,9 +9,9 @@ const MainPage: React.FC = () => {
  const { user, error, isLoading } = useUser();
   return (
     <div>
-      <h1>Welcome! {user?.name}</h1>
-      {/* <a href="/api/auth/logout">Logout</a> couldn't make it work - I guess GET method to /v2/logout endpoint is necessary*/}
-    </div>
+    <h2>Welcome {user?.name}</h2>
+    <p>Your email address is: {user?.email}</p>
+  </div>
   );
 };
 
