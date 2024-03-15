c62188a0cff0ebde05a1751ec07462ff1ad3268f
Shrey Ajaykumar Bhatt <sabhatt2@myseneca.ca>
2024-03-15 18:26:30 -0400

    changing lease creation

--- client/src/app/api/leases/route.ts
+++ client/src/app/api/leases/route.ts
@@ -2,24 +2,11 @@
 import { NextRequest, NextResponse } from 'next/server'; 
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import clientPromise from "../../../../mongodb/mongodb";
+import { User, Lease } from "../../../../mongodb/schemas";
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI || "");
-
-let leaseSchema = new mongoose.Schema({
-    rentalAddress: String,
-    city: String,
-    postalcode: String,
-    province: String,
-    landlordName: String,
-    tenantName: String,
-    rentAmount: Number,
-    docIDs: [{type: mongoose.Schema.Types.ObjectId, ref: 'Document'}]
-  });
-  
-let Lease = mongoose.models.leases || mongoose.model('leases', leaseSchema);
 
 // query leases collection based on user email
 export async function GET(req: NextRequest, res: NextResponse) {
@@ -27,15 +14,11 @@ export async function GET(req: NextRequest, res: NextResponse) {
         const urlParams = new URLSearchParams(req.url.split('?')[1])
         const email = urlParams.get('email')
         console.log(email);
-        const client = await clientPromise;
-        const db = client.db("test");
-        
-        // get user
-        const user = await db.collection("users").findOne({ email });
+
+        const user = await User.find({email:email});
         console.log(user);
 
-        // get leases from references
-        const leases = await db.collection("leases").find({ _id: { $in: user?.leaseIDs } }).toArray();
+        const leases = await Lease.find({ _id: { $in: user.leaseIDs } }).toArray();
         console.log(leases);
 
         return NextResponse.json(leases, { status: 200 });
@@ -48,7 +31,7 @@ export async function GET(req: NextRequest, res: NextResponse) {
 // create new lease
 export async function POST(req: NextRequest, res: NextResponse) {
     try { 
-        const { rentalAddress, city, postalcode, province, landlordName, tenantName , rentAmount, docIDs } = await req.json();
+        const { rentalAddress, city, postalcode, province, landlordName, tenantName , rentAmount, docIDs, landlordEmail } = await req.json();
 
         let lease = new Lease({
             rentalAddress: rentalAddress,
@@ -56,6 +39,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
             postalcode: postalcode,
             province: province,
             landlordName: landlordName,
+            landlordEmail : landlordEmail
             tenantName: tenantName,
             rentAmount: Number(rentAmount),
             docIDs: docIDs,
@@ -67,43 +51,20 @@ export async function POST(req: NextRequest, res: NextResponse) {
         })
         .catch((err: any) => {
             console.log(err);
+            return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
         });
+
+        const lease_id = Lease.find
+
+        //update users
+        User.updateOne({email:landlordEmail}, {$push : {leaseIDs : }})
+
         return NextResponse.json({ success:"ok" }, { status: 200 })
     } catch (e) {
         console.log("can't post: ", e);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
     }
 }
-
-/* export async function POST(req: NextRequest, res: NextResponse) {
-    console.log(req.body);
-    try { 
-        const { rentalAddress, city, postalcode, province, landlordName, tenantName, rentAmount } = await req.json();
-        console.log(rentalAddress);
-    
-        const client = await clientPromise;
-        const db = client.db(process.env.MONGODB_DB);
-        const collection = db.collection('leases');
-        const  result = {
-            rentalAddress: rentalAddress,
-            city: city,
-            postalcode,
-            province,
-            landlordName,
-            tenantName,
-            rentAmount
-        }
-
-        await collection.insertOne(result);
-    
-        //const result = await collection.insertOne({ rentalAddress, city, postalcode, province, landlordName, tenantName, rentAmount});
-    
-        return NextResponse.json({ success: 'ok' }, { status: 200 })
-    } catch (e) {
-        console.log("can't post: ", e);
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
-    }
-  } */
 
 export const config = {
     api: {
